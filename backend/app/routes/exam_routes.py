e3b8663b0e2993f6bf4aa6b064b7a8117f8787cc
Kunal Patidar <86792267+kunalpatida@users.noreply.github.com>
2026-04-11 22:33:33 +0530

    Update exam_routes.py

--- backend/app/routes/exam_routes.py
+++ backend/app/routes/exam_routes.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from datetime import datetime, timezone
 import random
 
 from database import get_db_connection
